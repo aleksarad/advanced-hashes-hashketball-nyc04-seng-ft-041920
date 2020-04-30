77b5dee6f6c2deb35edfc4111fc1e61c0899c295
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-30 18:17:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -181,7 +181,8 @@ end
 def player_stats (player)
   game_hash.each do |home_away, outer_value|
     outer_value[:players].each do |key, inner_val|
-      if key[:player_name] == player
+      if key[:player_name] == player 
+      end
     end
   end
 end
