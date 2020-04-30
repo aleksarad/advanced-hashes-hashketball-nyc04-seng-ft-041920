f0e2067440b6cb9e4a48de0ec426bd82a81214ff
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-30 18:11:26 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -171,7 +171,7 @@ def player_numbers (team_name)
   game_hash.each do |home_away, outer_value|
     if outer_value[:team_name] == team_name
       outer_value[:players].each do |key, inner_val|
-        team_numbers.push(k[:number])
+        team_numbers.push(key[:number])
       end
     end
   end
