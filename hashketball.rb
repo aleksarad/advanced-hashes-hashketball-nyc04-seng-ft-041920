66e74be6aa7e0e5f2ff5562406a0d229866fe23e
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-30 18:32:38 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -191,7 +191,7 @@ end
 
 def big_shoe_rebounds
   game_hash.each do |home_away, outer_value|
-    outer_value[:players].each do |k, v|
+    outer_value[:players].each do |player|
      pp k
     end
   end
