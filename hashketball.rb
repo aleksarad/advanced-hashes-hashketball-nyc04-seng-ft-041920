21cf584751012620ba895423a404c99c60ef11da
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-30 18:38:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -190,7 +190,7 @@ def player_stats (player)
 end
 
 def big_shoe_rebounds
-  shoe_size = 0
+  biggest_shoe = 0
   game_hash.each do |home_away, outer_value|
     outer_value[:players].each do |player|
       if player[:shoe] > shoe_size
